a598860df706d9d8693590aef8c64af7da4bf85d
Jayden Webb <jayden.webb888@gmail.com>
2025-02-23 01:02:31 -0500

    Fixing loading bug

--- webapp/frontend/src/pages/index.tsx
+++ webapp/frontend/src/pages/index.tsx
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { onAuthStateChanged } from "firebase/auth";
+import LandingPage from "./landingpage";
 import { auth } from "../../firebase-safe";
 
 export default function Home() {
-  return <div>Loading...</div>;
+  return <LandingPage />;
 }
--- webapp/frontend/src/pages/landingpage.tsx
+++ webapp/frontend/src/pages/landingpage.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { TextHoverEffect } from "@/components/ui/text-hover-effect";
+import { Button } from "@/components/ui/button";
+import { c } from "node_modules/framer-motion/dist/types.d-6pKw1mTI";
+import { useRouter } from "next/router";
 
 export default function LandingPage() {
-    return (
-        <div className="w-full h-screen flex flex-col items-center justify-center">
-            <div className="h-[40rem] flex items-center justify-center absolute">
-                <TextHoverEffect text="CLEAR" />
-            </div>
-        </div >
-    );
-}   
+  return (
+    <>
+      <div className="w-full h-screen flex flex-col items-center justify-center">
+        <div className="h-[40rem] flex items-center justify-center absolute">
+          <TextHoverEffect text="CLEAR" />
+        </div>
+      </div>
+      <Button
+        onClick={() => {
+          const router = useRouter();
+          router.push("/loginPage");
+        }}
+      >
+        {" "}
+        Discover clarity{" "}
+      </Button>
+    </>
+  );
+}
